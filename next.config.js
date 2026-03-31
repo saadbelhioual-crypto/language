517c5580275f0cb0f3bf540d085c58f5ed839022
saadbelhioual-crypto <saadbelhioual@gmail.com>
2026-03-31 21:34:20 +0100

    Add TypeScript configuration to ignore build errors
    Configure Next.js to ignore TypeScript build errors.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // سيقوم بتجاهل أخطاء التايب سكريبت أثناء الـ Build
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
